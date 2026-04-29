33c3d4a4861822d6fd71fdcb89b160afa27872cd
thomascarneiro74 <thomascarneiroinf@gmail.com>
2026-04-29 19:45:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/signaturevan">
+      <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
